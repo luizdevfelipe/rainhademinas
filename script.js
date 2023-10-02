9a4166a87655cc678e5b4b789015c44b086549fe
luizdevfelipe <lipefavaro2005@gmail.com>
2023-10-02 16:30:11 -0300

    menu funcionando em outras páginas

---
+++ script.js
@@ -0,0 +1,15 @@
+function mostraMenu(){
+    if (menu.style.display == 'block'){
+        menu.style.display = 'none'
+    } else {
+        menu.style.display = 'block'
+    }
+}
+
+function tamanho_tela(){
+    if (window.innerWidth >= 992){
+        menu.style.display = 'block'
+    } else {
+        menu.style.display = 'none'
+    }
+}
