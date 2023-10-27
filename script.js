477785084fc20c0adb6c6917a4e9fdc68ad627af
luizdevfelipe <lipefavaro2005@gmail.com>
2023-10-27 15:18:57 -0300

    carrossel com parallax

--- script.js
+++ script.js
@@ -2,13 +2,3 @@ function mensagem(){
     alert('Mensagem Enviada!')
    
 }
-
-function carrosel(){
-    f1 = document.getElementById('primeiro')
-    f2 = document.getElementById('segundo')
-    f3 = document.getElementById('terceiro')
-
-    if (window.innerWidth >= 992){
-        f1.src = "imagens/carrosel/cesta.jpg"
-    }
-}
